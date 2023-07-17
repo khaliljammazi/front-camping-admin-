434924df870a8dbab1d7e8c52326e3704c1fb577
BalssemZoghbi <zoghbibalssem@gmail.com>
2023-07-17 23:37:44 +0100

    reservation client side

---
+++ src/app/client-side/pages/reservation/reservation.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReservationRoutingModule } from './reservation-routing.module';
+import { ReservationComponent } from './reservation.component';
+import { PageTitleModule } from 'src/app/shared/page-title/page-title.module';
+import { QuillModule } from 'ngx-quill';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { Select2Module } from 'ng-select2-component';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@NgModule({
+  declarations: [
+    ReservationComponent
+  ],
+  imports: [
+    CommonModule,
+    ReservationRoutingModule,
+    PageTitleModule,
+    QuillModule,
+    NgxDropzoneModule,
+    Select2Module,
+    NgbTooltipModule
+  ]
+})
+export class ReservationModule { }
